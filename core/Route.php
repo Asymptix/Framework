7db83239c853eff82975273f601d0d06979d6865
Dmytro P. Zarezenko <dmytro.zarezenko@gmail.com>
2015-03-10 14:25:18 +0200

    Main route functionality class initial commit.

---
+++ core/Route.php
@@ -0,0 +1,108 @@
+<?php
+
+require_once("core/Tools.php");
+
+/**
+ * Main route functionality class.
+ *
+ * @category Asymptix PHP Framework
+ * @author Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
+ * @copyright (c) 2011 - 2015, Dmytro Zarezenko
+ * @license http://opensource.org/licenses/MIT
+ */
+class Route {
+
+    public $controller = null;
+    public $action = null;
+    public $id = null;
+    public $customFields = array();
+
+    public $isBackend = false;
+
+    /**
+     * Constructor that creates Route object from URL request string.
+     *
+     * @param string $request URL request string without GET params.
+     */
+    public function Route($request) {
+        $route = array_values(array_filter(explode("/", $request)));
+
+        // result array must contain keys starts from 0
+        if (isset($route[0]) && $route[0] == "admin") {
+            array_shift($route);
+            $this->isBackend = true;
+        }
+        if (count($route) < 3) {
+            $route = array_merge($route, array_fill(0, 3 - count($route), ""));
+        } else {
+            $route = array_values($route);
+        }
+
+        list($this->controller, $this->action, $this->id) = $route;
+
+        if (count($route) > 3) {
+            $this->customFields[] = array_slice($route, 3);
+        }
+    }
+
+    /**
+     * Detects if current Route controller present in aliases list (used for
+     * example for Menu functionality).
+     *
+     * @param array<string> $aliases List of aliases.
+     *
+     * @return boolean
+     */
+    public function isOneOf($aliases) {
+        return in_array($this->controller, $aliases);
+    }
+
+    /**
+     * Detects current controller action.
+     *
+     * @param string $actionFieldName $_REQUEST action field name if form submit.
+     * @param string $defaultAction Default action if no action detected.
+     *
+     * @return string Action name.
+     */
+    public function getAction($actionFieldName = 'action', $defaultAction = 'list') {
+        $action = Tools::getFieldValue('action');
+        if (empty($action)) {
+            if (!empty($this->action)) {
+                return $this->action;
+            } else {
+                return $defaultAction;
+            }
+        }
+        return $action;
+    }
+
+    /**
+     * Generates URL for current Route with action, id and GET params.
+     *
+     * @param string $action Action.
+     * @param mixed $id Id of the record (optional).
+     * @param array $getParams List of GET params (optional).
+     *
+     * @return string URL.
+     */
+    public function getUrl($action, $id = null, $getParams = array()) {
+        $url = $this->controller . "/";
+        if (!is_null($id)) {
+            $url.= $action . "/" . $id;
+        } else {
+            $url.= $action;
+        }
+
+        if (!empty($getParams)) {
+            $url.= "?";
+            foreach ($getParams as $key => $value) {
+                $url.= $key . "=" . $value;
+            }
+        }
+        return $url;
+    }
+
+}
+
+?>
