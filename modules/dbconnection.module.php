37cb44746fb1d7e1f9e47b0c57be9b056445fbe8
Dmytro P. Zarezenko <dmytro.zarezenko@gmail.com>
2015-04-11 17:09:26 +0300

    1. modules/ folder added for framework modules lib 2. Database connection module added

---
+++ modules/dbconnection.module.php
@@ -0,0 +1,34 @@
+<?php
+
+$mysqli = new mysqli(
+        Config::getDBConfigParam('HOST'),
+        Config::getDBConfigParam('USER'), Config::getDBConfigParam('PASSWORD'),
+        Config::getDBConfigParam('DBNAME'));
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+} else {
+    //change character set to utf8
+    if (!$mysqli->set_charset(Config::DB_CHARSET)) {
+        printf("Error loading character set " . Config::DB_CHARSET . ": %s\n", $mysqli->error);
+    } else {
+        //printf("Current character set: %s\n", $mysqli->character_set_name());
+    }
+
+    $manager = DBCore::getInstance();
+
+    DBCore::connection($mysqli);
+    //$manager->setCurrentConnection('first');
+}
+
+// Register a shutdown function which will close DB connection
+function onExit() {
+    global $manager;
+    $conns = $manager->getConnections();
+    foreach ($conns as $connName => $connResource) { // TODO: without name
+        $manager->closeConnection($connResource);
+    }
+}
+register_shutdown_function("onExit");
+
+
+?>
