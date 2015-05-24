81ac4404ead109a76f404ab25557f056400072f8
Dmytro P. Zarezenko <dmytro.zarezenko@gmail.com>
2015-05-24 17:44:43 +0300

    Git repository URL added to the class PHPDoc

--- core/Tools.php
+++ core/Tools.php
@@ -6,6 +6,8 @@
  * @category Asymptix PHP Framework
  * @author Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
  * @copyright (c) 2009 - 2015, Dmytro Zarezenko
+ *
+ * @git https://github.com/dzarezenko/Asymptix-PHP-Framework.git
  * @license http://opensource.org/licenses/MIT
  */
 class Tools {
