7610840daea7bfaa07a0102bcb3e19b5502886e5
Dmytro P. Zarezenko <dmytro.zarezenko@gmail.com>
2015-08-06 17:44:21 +0300

    Localization module added and this functionality separated from Localization class.

---
+++ modules/localization.module.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Works with language in session and POST requests (to set language).
+ *
+ * @category Asymptix PHP Framework
+ * @author Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
+ * @copyright (c) 2009 - 2015, Dmytro Zarezenko
+ *
+ * @git https://github.com/dzarezenko/Asymptix-PHP-Framework.git
+ * @license http://opensource.org/licenses/MIT
+ */
+
+use Asymptix\Localization\Languages;
+
+if (isset($_POST['lang'])) {
+    $_SESSION['lang'] = $_POST['lang'];
+} elseif (!isset($_SESSION['lang'])) {
+    $_SESSION['lang'] = 'en';
+}
+
+$_LANG = Languages::getLanguage($_SESSION['lang']);
+$_SESSION['lang'] = $_LANG->code;
+
+require_once(realpath(dirname(__FILE__)) . "/../conf/langs/" . $_LANG->code . ".php");
+
+?>
