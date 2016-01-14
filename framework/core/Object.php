d38ce8223643ccbfdc5aac702363d73e89ff1985
Dmytro P. Zarezenko <dmytro.zarezenko@gmail.com>
2016-01-15 00:34:40 +0200

    1. Fields names aliases functionality improved and enhanced. 2. PHPDocs added.

--- framework/core/Object.php
+++ framework/core/Object.php
@@ -17,7 +17,7 @@ abstract class Object {
     /**
      * List of the database entity fields.
      *
-     * @var array<mixed>
+     * @var array
      */
     protected $fieldsList;
 
@@ -27,11 +27,6 @@ abstract class Object {
      * @var array
      */
     protected $fieldsAliases;
-
-    /**
-     * Create new default object.
-     */
-    public function Object() {}
 
     /**
      * Sets values of the object fields.
@@ -47,19 +42,28 @@ abstract class Object {
                 if (isset($valuesList[$fieldName])) {
                     $newValue = $valuesList[$fieldName];
                     if (empty($newValue)) {
-                        if (is_integer($fieldValue)) {
-                            $fieldValue = 0;
-                        } elseif (is_string($fieldValue)) {
-                            $fieldValue = "";
-                        } elseif (is_null($fieldValue)) {
-                            $fieldValue = null;
-                        } else {
-                            $fieldValue = $newValue;
-                        }
+                        $fieldValue = self::getEmptyValue($fieldValue, $newValue);
                     } else {
                         $fieldValue = $newValue;
                     }
                     $count ++;
+                } else { // look up for the field aliases
+                    $fieldAliases = array_keys($this->fieldsAliases, $fieldName);
+                    if (!empty($fieldAliases)) {
+                        foreach ($fieldAliases as $alias) {
+                            if (isset($valuesList[$alias])) {
+                                $newValue = $valuesList[$alias];
+                                if (empty($newValue)) {
+                                    $fieldValue = self::getEmptyValue($fieldValue, $newValue);
+                                } else {
+                                    $fieldValue = $newValue;
+                                }
+                                $count ++;
+
+                                break;
+                            }
+                        }
+                    }
                 }
             }
             return $count;
@@ -94,18 +98,30 @@ abstract class Object {
     /**
      * Returns fields list array.
      *
+     * @param bool $withAliases If this flag is `true` then we will have fields
+     *           aliases in the result array as well.
+     *
      * @return array
      */
-    public function getFieldsList() {
+    public function getFieldsList($withAliases = false) {
+        if ($withAliases && !empty($this->fieldsAliases)) {
+            $fieldsList = $this->fieldsList;
+            foreach ($this->fieldsAliases as $alias => $fieldName) {
+                $fieldsList[$alias] = $this->getFieldValue($alias);
+            }
+
+            return $fieldsList;
+        }
         return $this->fieldsList;
     }
 
     /**
-     * TODO: add docs
+     * Returns value of the field by it's name or alias.
      *
-     * @param type $fieldName
-     * @return type
-     * @throws \Exception
+     * @param string $fieldName Field name or alias.
+     *
+     * @return mixed
+     * @throws \Exception If object doesn't have this field or alias.
      */
     public function getFieldValue($fieldName) {
         if (isset($this->fieldsAliases[$fieldName])) {
@@ -117,6 +133,25 @@ abstract class Object {
         } else {
             throw new \Exception("Object '" . get_class($this) . "' hasn't field '" . $fieldName . "'");
         }
+    }
+
+    /**
+     * Returns type custed new empty field value.
+     *
+     * @param mixed $fieldValue Current field value.
+     * @param mixed $newValue New value.
+     *
+     * @return mixed
+     */
+    private static function getEmptyValue($fieldValue, $newValue) {
+        if (is_integer($fieldValue)) {
+            return 0;
+        } elseif (is_string($fieldValue)) {
+            return "";
+        } elseif (is_null($fieldValue)) {
+            return null;
+        }
+        return $newValue;
     }
 
     /**
