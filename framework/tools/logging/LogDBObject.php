9a83c845cedd854859c9e1b273cb0c7abf42f956
Dmytro P. Zarezenko <dmytro.zarezenko@gmail.com>
2016-06-29 17:10:05 +0300

    Universal Logging functionality added. Supports browser output, file and database logging.

---
+++ framework/tools/logging/LogDBObject.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Asymptix\tools\logging;
+
+use Asymptix\db\DBCore;
+
+/**
+ * Simple Log class to store Logger messages into teh database.
+ *
+ * @category Asymptix PHP Framework
+ * @author Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
+ * @copyright (c) 2016, Dmytro Zarezenko
+ * @license http://opensource.org/licenses/MIT
+ */
+class LogDBObject extends \Asymptix\db\DBObject {
+
+    const TABLE_NAME = "log";
+    const ID_FIELD_NAME = "";
+    protected $fieldsList = array(
+        'type' => "", // varchar(10), default ''
+        'message' => "", // text, not null, default ''
+        'count' => 0, // int(6) unsigned, not null, default ''
+        'last_seen' => "CURRENT_TIMESTAMP", // datetime, not null, default 'CURRENT_TIMESTAMP'
+    );
+
+    /**
+     * Log method, must be in all classes for Logger class functionality.
+     *
+     * @param string $type Log message type from Logger class.
+     * @param string $message Message text.
+     * @param int $time Timestamp.
+     *
+     * @return int Number of affected rows.
+     * @throws \Asymptix\db\DBCoreException If some database error occurred.
+     */
+    public static function log($type, $message, $time = null) {
+        if (is_null($time)) {
+            $time = time();
+        }
+
+        $query = "INSERT INTO " . self::TABLE_NAME . " (type, message, count, last_seen) VALUES (?, ?, 1, ?)
+                  ON DUPLICATE KEY UPDATE count = count + 1, last_seen = ?";
+        try {
+            return DBCore::doUpdateQuery($query, "ssss", array(
+                $type, $message, date("Y-m-d H:i:s", $time), date("Y-m-d H:i:s", $time)
+            ));
+        } catch (\Asymptix\db\DBCoreException $e) {
+            print($e->getMessage());
+        }
+    }
+
+}
---
+++ framework/tools/logging/Logger.php
@@ -0,0 +1,206 @@
+<?php
+
+namespace Asymptix\tools\logging;
+
+use Asymptix\core\OutputStream;
+
+/**
+ * Universal Logging functionality. Supports browser output, file and database
+ * logging.
+ *
+ * @category Asymptix PHP Framework
+ * @author Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
+ * @copyright (c) 2016, Dmytro Zarezenko
+ * @license http://opensource.org/licenses/MIT
+ */
+class Logger {
+
+    /**
+     * Log messages types constants.
+     */
+    const LOG_INFO    = "info";
+    const LOG_DEBUG   = "debug";
+    const LOG_SUCCESS = "success";
+    const LOG_WARNING = "warning";
+    const LOG_ERROR   = "error";
+
+    /**
+     * Log messages output directions constants.
+     */
+    const TO_OUTPUT_STREAM = 0;
+    const TO_FILE = 1;
+    const TO_DB = 2;
+
+    /**
+     * Log messages output direction.
+     *
+     * @var int
+     */
+    private $direction = null;
+
+    /**
+     * Log file name.
+     *
+     * @var string
+     */
+    private $fileName = null;
+
+    /**
+     * Log DB object. Object must have log() method for logging functionality
+     * support.
+     *
+     * @var \Asymptix\db\DBObject
+     */
+    private $dbObject = null;
+
+    /**
+     * Initiates Logger setting and starts logging.
+     *
+     * @param int $direction Output direction.
+     * @param mixed $output Output file name od DB object.
+     *
+     * @throws LoggerException
+     */
+    public function __construct($direction = null, $output = null) {
+        switch ($direction) {
+            case (null):
+            case (self::TO_OUTPUT_STREAM):
+                $this->direction = self::TO_OUTPUT_STREAM;
+                break;
+            case (self::TO_FILE):
+                $this->direction = self::TO_FILE;
+                $this->fileName = $output;
+                break;
+            case (self::TO_DB):
+                $this->direction = self::TO_DB;
+                $this->dbObject = $output;
+                break;
+            default:
+                throw new LoggerException("Invalid logging output direction type");
+        }
+        $this->start();
+    }
+
+    /**
+     * Performs preparation to the logging process.
+     *
+     * @throws LoggerException
+     */
+    public function start() {
+        switch ($this->direction) {
+            case (self::TO_OUTPUT_STREAM):
+                OutputStream::start();
+
+                return;
+            case (self::TO_FILE):
+                if (empty($this->fileName)) {
+                    throw new LoggerException("Log file name is invalid");
+                }
+                if (file_exists($this->fileName)) {
+                    if (!is_file($this->fileName)) {
+                        throw new LoggerException("Log file name is invalid");
+                    }
+                }
+                if (file_put_contents($this->fileName, "", FILE_APPEND) === false) {
+                    throw new LoggerException("Can't write to a file");
+                }
+
+                return;
+            case (self::TO_DB):
+                if (empty($this->dbObject) || !is_object($this->dbObject)) {
+                    throw new LoggerException("Invalid LogDBObject object");
+                }
+                if (!method_exists($this->dbObject, "log")) {
+                    throw new LoggerException("No log() method in the LogDBObject object");
+                }
+
+                return;
+            default:
+                throw new LoggerException("Invalid logging output direction type");
+        }
+    }
+
+    /**
+     * Main logging method, performes log writing.
+     *
+     * @param int $type Log message type.
+     * @param string $message Message text.
+     * @param string $format Time label format.
+     * @param int $time Timestamp.
+     *
+     * @throws LoggerException
+     */
+    public function log($type, $message, $format = "\[Y-m-d H:i:s\]", $time = null) {
+        $msgType = null;
+        switch ($type) {
+            case (self::LOG_INFO):
+                $msgType = OutputStream::MSG_INFO;
+                break;
+            case (self::LOG_DEBUG):
+                $msgType = OutputStream::MSG_DEBUG;
+                break;
+            case (self::LOG_SUCCESS):
+                $msgType = OutputStream::MSG_SUCCESS;
+                break;
+            case (self::LOG_WARNING):
+                $msgType = OutputStream::MSG_WARNING;
+                break;
+            case (self::LOG_ERROR):
+                $msgType = OutputStream::MSG_ERROR;
+                break;
+            default:
+                throw new LoggerException("Invalid message type");
+        }
+
+        switch ($this->direction) {
+            case (self::TO_OUTPUT_STREAM):
+                OutputStream::msg($msgType, $message, $format, $time);
+
+                return;
+            case (self::TO_FILE):
+                $message = "({$type}) " . $message;
+
+                if (strpos($message, "{{time}}") === false) {
+                    $message = "{{time}} " . $message;
+                }
+                if (is_null($time)) {
+                    $time = time();
+                }
+                $message = str_replace("{{time}}", date($format, $time), $message);
+
+                file_put_contents($this->fileName, "{$message}\n", FILE_APPEND);
+
+                return;
+            case (self::TO_DB):
+                $this->dbObject->log($type, $message, $time = null);
+
+                return;
+            default:
+                throw new LoggerException("Invalid logging output direction type");
+        }
+    }
+
+    /**
+     * Closes Logger session.
+     *
+     * @throws LoggerException
+     */
+    public function close() {
+        switch ($this->direction) {
+            case (self::TO_OUTPUT_STREAM):
+                OutputStream::close();
+                return;
+            case (self::TO_FILE):
+                // nothing to do
+                return;
+            case (self::TO_DB):
+                // nothing to do
+                return;
+            default:
+                throw new LoggerException("Invalid logging output direction type");
+        }
+    }
+
+}
+
+class LoggerException extends \Exception {}
