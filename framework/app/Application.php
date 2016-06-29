65361bf32cce6471c1556c2b21223b078ca8abd6
Dmytro P. Zarezenko <dmytro.zarezenko@gmail.com>
2016-06-29 11:56:53 +0300

    Application class simple prototype added.

---
+++ framework/app/Application.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Asymptix\app;
+
+/**
+ *
+ *
+ * @author Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
+ */
+class Application {
+
+    public static $controller = null;
+    public static $view = null;
+
+}
