1815ed60df993018629731c8b9acc97a881ed050
Dmytro P. Zarezenko <dmytro.zarezenko@gmail.com>
2017-02-22 22:30:33 +0200

    validateUrl() method now works via isUrl() method.

--- framework/core/Validator.php
+++ framework/core/Validator.php
@@ -239,7 +239,7 @@ class Validator {
             Errors::saveErrorFor($fieldName, \__ERRORS::URL_EMPTY);
 
             return false;
-        } elseif (!self::validateRegexp($fieldValue, "<^(?#Protocol)(?:(?:ht|f)tp(?:s?)\:\/\/|~/|/)?(?#Username:Password)(?:\w+:\w+@)?(?#Subdomains)(?:(?:[-\w]+\.)+(?#TopLevel Domains)(?:com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum|travel|[a-z]{2}))(?#Port)(?::[\d]{1,5})?(?#Directories)(?:(?:(?:/(?:[-\w~!$+|.,=]|%[a-f\d]{2})+)+|/)+|\?|#)?(?#Query)(?:(?:\?(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)(?:&(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)*)*(?#Anchor)(?:#(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)?$>")) {
+        } elseif (!self::isUrl($fieldValue)) {
             Errors::saveErrorFor($fieldName, \__ERRORS::URL_INVALID);
 
             return false;
