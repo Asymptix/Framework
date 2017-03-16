8760cf596b143146bb87fb985a201cd6f046e855
Dmytro P. Zarezenko <dmytro.zarezenko@gmail.com>
2017-03-16 15:09:14 +0200

    PHPUnit namespaces backward compatibility enhanced.

--- tests/autoload.php
+++ tests/autoload.php
@@ -37,3 +37,8 @@ if (!class_exists('\PHPUnit\Framework\TestCase') &&
     class_exists('\PHPUnit_Framework_TestCase')) {
     class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
 }
+
+if (!class_exists('\PHPUnit_Framework_TestCase') &&
+    class_exists('\PHPUnit\Framework\TestCase')) {
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+}
